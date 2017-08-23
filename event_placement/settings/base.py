43e7444559322ea387eed27f5a6067b86ac4449f
CrateC <artem.boyko@gmail.com>
2017-08-23 05:09:28 +0300

    make better

--- event_placement/settings/base.py
+++ event_placement/settings/base.py
@@ -153,7 +153,7 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 # STATIC_ROOT = 'C:/Python/Training/django-core/django-events-placement/src/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
