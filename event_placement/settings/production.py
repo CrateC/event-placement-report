f8e108a9bf5dd371e6f8aa8f2efe2f4609d25ccd
CrateC <artem.boyko@gmail.com>
2017-08-23 05:12:05 +0300

    make better

--- event_placement/settings/production.py
+++ event_placement/settings/production.py
@@ -34,8 +34,6 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-    'compressor.finders.CompressorFinder',
 )
 
 
