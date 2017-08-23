b27f15d013f1616453f2afaa736f72edaaddb7dd
CrateC <artem.boyko@gmail.com>
2017-08-23 04:43:54 +0300

    production settings

--- event_placement/settings/production.py
+++ event_placement/settings/production.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 # List of finder classes that know how to find static files in
 # various locations.
